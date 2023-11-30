67207977307f14512040ea8b17e07b74fc4e9f4a
Maria <mengozzi.maria@gmail.com>
2023-11-30 13:45:26 +0100

    feat: tutorial p2

---
+++ miniproject/moz-todo-vue/src/components/ToDoItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" :checked="isDone"/>
+    <label for="todo-item" :for="id">{{label}}</label>
+  </div>
+</template>
+
+<script>
+    import uniqueId from "loadash.uniqueid";
+    export default{
+        props: {
+            label: { 
+                required: true, 
+                type: String
+            }
+            done: {
+                default: false,
+                type: Boolean
+            },
+        },
+        data() {
+            return {
+                isDone: this.done,
+                id: uniqueId("todo-"),
+            };
+        },
+    };
+</script>
